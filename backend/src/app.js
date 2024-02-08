3da28cf555529f8724673eccbac5c3c8830b1020
ArthurHtbk <78815489+ArthurHtbk@users.noreply.github.com>
2024-02-08 17:22:57 +0100

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -105,7 +105,7 @@ app.use("/api", router);
 // 1. Uncomment the lines related to serving static files and redirecting unhandled requests.
 // 2. Ensure that the `reactBuildPath` points to the correct directory where your frontend's build artifacts are located.
 
-const reactBuildPath = path.resolve(`${__dirname}/../../frontend/dist`);
+const reactBuildPath = `${__dirname}/../../frontend/dist`;
 
 // Serve react resources
 
@@ -114,7 +114,7 @@ app.use(express.static(reactBuildPath));
 // Redirect unhandled requests to the react index file
 
 app.get("*", (req, res) => {
-  res.sendFile(`${reactBuildPath}/index.html`);
+  res.sendFile("index.html", { root: reactBuildPath });
 });
 
 /* ************************************************************************* */
